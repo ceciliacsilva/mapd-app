bacb397a715cb420222ba882d09775c8c002cdba
Cecilia Carneiro <cecilia.carneiroesilva@gmail.com>
2019-10-03 15:39:59 -0300

    js-vega working - uhulll

--- js-vega/main.ts
+++ js-vega/main.ts
@@ -12,7 +12,9 @@ const connection = new (window as any).MapdCon()
 
 const table = "flights_2008_10k";
 
+var vegaOptions = {}
 connection.connectAsync().then(session => {
+
     // assign session to OmniSci Core transform
     QueryCore.session(session);
     
@@ -26,19 +28,19 @@ connection.connectAsync().then(session => {
         .initialize(document.querySelector("#view"));
     
     view.runAsync();
-    
+
     // assign view and vega to window so we can debug them
     window["vega"] = vega;
     window["view"] = view;
 });
 
 // transform to compute the extent
-const extent = {
-    type: "querycore",
-    query: {
-        signal: `'select min(' + field + ') as "min", max(' + field + ') as "max" from ${table}'`
-    }
-} as any;
+// const extent = {
+//     type: "querycore",
+//     query: {
+//         signal: `'select min(' + field + ') as "min", max(' + field + ') as "max" from ${table}'`
+//     }
+// } as any;
 
 // bin and aggregate
 const data = {
@@ -48,142 +50,118 @@ const data = {
     }
 } as any;
 
+const extent = {
+    type: "querycore",
+    query: "select origin_state as category, count(origin_state) as amount from flights_2008_10k GROUP by origin_state limit 10"
+} as any;
+
 const spec: vega.Spec = {
-    $schema: "https://vega.github.io/schema/vega/v5.json",
-    autosize: "pad",
-    padding: 5,
-    width: 600,
-    height: 250,
-    signals: [
-        {
-            name: "field",
-            value: "airtime",
-            bind: {
-                input: "select",
-                options: [
-                    "deptime",
-                    "crsdeptime",
-                    "arrtime",
-                    "crsarrtime",
-                    "flightnum",
-                    "actualelapsedtime",
-                    "crselapsedtime",
-                    "airtime",
-                    "arrdelay",
-                    "depdelay",
-                    "distance",
-                    "taxiin",
-                    "taxiout",
-                    "carrierdelay",
-                    "weatherdelay",
-                    "nasdelay",
-                    "securitydelay",
-                    "lateaircraftdelay",
-                    "plane_year"
-                ]
-            }
+  "$schema": "https://vega.github.io/schema/vega/v5.json",
+  "width": 400,
+  "height": 200,
+  "padding": 5,
+
+  "data": [
+    // {
+    //     "name": "table",
+    //     // "values": [
+    //     //     { "amount": 28},
+    //     //     { "amount": 55},
+    //     //     { "amount": 43},
+    //     //     { "amount": 91},
+    //     //     { "amount": 81},
+    //     //     { "amount": 53},
+    //     //     { "amount": 19},
+    //     //     { "amount": 87}
+    //     // ]
+    //     // "values": [
+    //     //     {"category": "A", "amount": 28},
+    //     //     {"category": "B", "amount": 55},
+    //     //     {"category": "C", "amount": 43},
+    //     //     {"category": "D", "amount": 91},
+    //     //     {"category": "E", "amount": 81},
+    //     //     {"category": "F", "amount": 53},
+    //     //     {"category": "G", "amount": 19},
+    //     //     {"category": "H", "amount": 87}
+    //     // ]
+    // }
+      {
+          "name": "table",
+          "transform": [ extent ] 
+      }
+  ],
+
+  "signals": [
+    {
+      "name": "tooltip",
+      "value": {},
+      "on": [
+        {"events": "rect:mouseover", "update": "datum"},
+        {"events": "rect:mouseout",  "update": "{}"}
+      ]
+    }
+  ],
+
+  "scales": [
+    {
+      "name": "xscale",
+      "type": "band",
+      "domain": {"data": "table", "field": "category"},
+      "range": "width",
+      "padding": 0.05,
+      "round": true
+    },
+    {
+      "name": "yscale",
+      "domain": {"data": "table", "field": "amount"},
+      "nice": true,
+      "range": "height"
+    }
+  ],
+
+  "axes": [
+    { "orient": "bottom", "scale": "xscale" },
+    { "orient": "left", "scale": "yscale" }
+  ],
+
+  "marks": [
+    {
+      "type": "rect",
+      "from": {"data": "table"},
+      "encode": {
+        "enter": {
+          "x": {"scale": "xscale", "field": "category"},
+          "width": {"scale": "xscale", "band": 1},
+          "y": {"scale": "yscale", "field": "amount"},
+          "y2": {"scale": "yscale", "value": 0}
         },
-        {
-            name: "maxbins",
-            value: 20,
-            bind: { input: "range", min: 1, max: 300, debounce: 100 }
+        "update": {
+          "fill": {"value": "steelblue"}
+        },
+        "hover": {
+          "fill": {"value": "red"}
         }
-    ],
-    data: [
-        {
-            name: "extent",
-            transform: [extent]
+      }
+    },
+    {
+      "type": "text",
+      "encode": {
+        "enter": {
+          "align": {"value": "center"},
+          "baseline": {"value": "bottom"},
+          "fill": {"value": "#333"}
         },
-        {
-            name: "bin",
-            transform: [
-                // this bin transform doesn't actually bin any data, it just computea the bins signal
-                {
-                    type: "bin",
-                    field: null,
-                    signal: "bins",
-                    maxbins: { signal: "maxbins" },
-                    extent: {
-                        signal:
-                        "data('extent') ? [data('extent')[0]['min'], data('extent')[0]['max']] : [0, 0]"
-                    }
-                }
-            ]
-        },
-        {
-            name: "table",
-            transform: [
-                data,
-                {
-                    type: "formula",
-                    expr: "datum.bin_start + bins.step",
-                    as: "bin_end"
-                }
-            ]
+        "update": {
+          "x": {"scale": "xscale", "signal": "tooltip.category", "band": 0.5},
+          "y": {"scale": "yscale", "signal": "tooltip.amount", "offset": -2},
+          "text": {"signal": "tooltip.amount"},
+          "fillOpacity": [
+            {"test": "isNaN(tooltip.amount)", "value": 0},
+            {"value": 1}
+          ]
         }
-    ],
-    marks: [
-        {
-            name: "marks",
-            type: "rect",
-            from: { data: "table" },
-            encode: {
-                update: {
-                    fill: { value: "steelblue" },
-                    x2: {
-                        scale: "x",
-                        field: "bin_start",
-                        offset: {
-                            signal: "(bins.stop - bins.start)/bins.step > 150 ? 0 : 1"
-                        }
-                    },
-                    x: { scale: "x", field: "bin_end" },
-                    y: { scale: "y", field: "cnt" },
-                    y2: { scale: "y", value: 0 }
-                }
-            },
-        },
-    ],
-    scales: [
-        {
-            name: "x",
-            type: "linear",
-            domain: {
-                signal: "[bins.start, bins.stop]"
-            },
-            range: [-20, { signal: "width" }],
-            zero: false,
-            bins: { signal: "bins" }
-        },
-        {
-            name: "y",
-            type: "linear",
-            domain: { data: "table", field: "cnt" },
-            range: [{ signal: "height" }, 0],
-            nice: true,
-            zero: true
-        }
-    ],
-    axes: [
-        {
-            scale: "x",
-            orient: "bottom",
-            grid: false,
-            title: {
-                signal: `field`
-            },
-            labelFlush: true,
-            labelOverlap: true
-        },
-        {
-            scale: "y",
-            orient: "left",
-            grid: true,
-            title: "Count of Records",
-            labelOverlap: true,
-            gridOpacity: 0.7
-        }
-    ],
-    config: { axisY: { minExtent: 30 } }
-};
+      }
+    }
+  ]
+}
 
